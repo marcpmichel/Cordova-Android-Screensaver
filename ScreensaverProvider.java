39e328374d7f01c926b4d3371c2e40e7a254e8f1
Hugh Isaacs II <HughIsaacs2@users.noreply.github.com>
2017-04-28 18:41:36 -0400

    Update ScreensaverProvider.java
    Cleanup.

--- ScreensaverProvider.java
+++ ScreensaverProvider.java
@@ -1,22 +1,14 @@
 package com.hughisaacs2.cordova.plugins.androidscreensaverplugin;
 
-import android.annotation.TargetApi;
-import android.content.Intent;
-import android.service.dreams.DreamService;
-import android.view.View;
-import android.webkit.WebChromeClient;
-import android.webkit.WebView;
-
-import android.app.ActivityManager;
-import android.os.Build;
-import android.graphics.Color;
-import android.content.Context;
-
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaArgs;
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CordovaWebView;
+
+import android.annotation.TargetApi;
+import android.service.dreams.DreamService;
+import android.webkit.WebView;
 
 @TargetApi(17)
 public class ScreensaverProvider extends DreamService {
@@ -29,14 +21,13 @@ public class ScreensaverProvider extends DreamService {
         // Hide system UI
         setFullscreen(true);
         // Set the dream layout
-
         WebView webView = new WebView(this);
         setContentView(webView);
+
         webView.getSettings().setJavaScriptEnabled(true);
         webView.getSettings().setGeolocationEnabled(true);
         webView.getSettings().setDomStorageEnabled(true);
         webView.getSettings().setAllowFileAccess(true);
-
-        webView.loadUrl("file:///android_asset/www/screensaver.html");
+        webView.loadUrl("file:///android_asset/www/test.html");
     }
 }
