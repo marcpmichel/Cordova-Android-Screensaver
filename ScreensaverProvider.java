5634f82152f2c65d17523c673c26ac9d87092bb4
Hugh Isaacs II <HughIsaacs2@users.noreply.github.com>
2017-04-28 15:13:50 -0400

    Update ScreensaverProvider.java
    Added Cordova WebView.
    Attempting to add preferences stuff.

--- ScreensaverProvider.java
+++ ScreensaverProvider.java
@@ -3,10 +3,15 @@ package com.hughisaacs2.cordova.plugins.androidscreensaverplugin;
 import android.annotation.TargetApi;
 import android.service.dreams.DreamService;
 import android.webkit.WebView;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaArgs;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaWebView;
 import org.apache.cordova.*;
 
 @TargetApi(17)
-public class ScreensaverProvider extends DreamService {
+public class ScreensaverProvider extends DreamService implements CordovaInterface {
      @Override
      CordovaWebView cwv;
     public void onAttachedToWindow() {
@@ -16,12 +21,11 @@ public class ScreensaverProvider extends DreamService {
         // Hide system UI
         setFullscreen(true);
         // Set the dream layout
-	//WebView webView = new WebView(this);
-        //setContentView(webView);
-        //webView.loadUrl("file:///android_asset/www/screensaver.html");
-		cwv webview = new CordovaWebView(this);
-		org.apache.cordova.Config.init(this);
-        setContentView(cwv);
-        cwv.loadUrl(Config.getStartUrl());
+	
+        setContentView(R.layout.cordova_screen_saver);
+        cwv = (CordovaWebView) findViewById(R.id.CordovaScreenSaverWebView);
+
+        String screenSaverIndex = preferences.getString("ScreenSaverIndex", "index.html");
+        cwv.loadUrl(screenSaverIndex);
     }
 }
