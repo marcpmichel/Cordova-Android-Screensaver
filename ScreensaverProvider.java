fbc2b477c79d5f50f9b22683ae56b182444494cc
marcpmichel <marc.p.michel@gmail.com>
2021-07-30 10:59:54 +0200

    start app activity

--- ScreensaverProvider.java
+++ ScreensaverProvider.java
@@ -3,31 +3,30 @@ package com.hughisaacs2.cordova.plugins.androidscreensaverplugin;
 import android.annotation.TargetApi;
 import android.service.dreams.DreamService;
 import android.webkit.WebView;
-/*
-import org.apache.cordova.CallbackContext;
-import org.apache.cordova.CordovaArgs;
-import org.apache.cordova.CordovaInterface;
-import org.apache.cordova.CordovaPlugin;
-import org.apache.cordova.CordovaWebView;
-import org.apache.cordova.CordovaPreferences;
-*/
-@TargetApi(17)
+import android.content.Intent;
+
+@TargetApi(19)
 public class ScreensaverProvider extends DreamService {
      @Override
     public void onAttachedToWindow() {
         super.onAttachedToWindow();
-        // Allow user touch
-        setInteractive(true);
+        setInteractive(true); // Allow user touch
         // Hide system UI
         setFullscreen(true);
         // Set the dream layout
         WebView webView = new WebView(this);
          
         webView.getSettings().setJavaScriptEnabled(true);
-        //webView.getSettings().setGeolocationEnabled(true);
         webView.getSettings().setDomStorageEnabled(true);
-        //webView.getSettings().setAllowFileAccess(true);
         setContentView(webView);
         webView.loadUrl("file:///android_asset/www/screensaver.html");
+    }
+
+    public void onDreamingStarted() {
+		// start main activity
+		Intent intentDream = new Intent(Intent.ACTION_MAIN);
+		intentDream.setClassName("com.deepidoo.dream", "com.deepidoo.dream.MainActivity");
+		startActivity(intentDream);
+		finish(); // terminates the Dream
     }
 }
